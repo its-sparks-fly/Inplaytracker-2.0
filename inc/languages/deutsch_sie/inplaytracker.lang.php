2bc4084e46032d6c58aee0fc781d0eeb3a5812b5
Julia <midnightwhisper@web.de>
2017-10-25 00:07:49 +0200

    Create inplaytracker.lang.php

---
+++ inc/languages/deutsch_sie/inplaytracker.lang.php
@@ -0,0 +1,41 @@
+<?php
+
+$l['inplaytracker'] = "Inplaytracker";
+$l['inplaytracker_scenes'] = "Inplayszenen";
+$l['inplaytracker_posts'] = "Posts im Inplay";
+$l['inplaytracker_partner'] = "Postpartner";
+$l['inplaytracker_date'] = "Datum";
+$l['inplaytracker_location'] = "Ort";
+$l['inplaytracker_daytime'] = "Tageszeit";
+$l['inplaytracker_settings'] = "Szenen-Einstellungen";
+$l['inplaytracker_closed'] = "Private Szene";
+$l['inplaytracker_open'] = "Öffentliche Szene";
+$l['inplaytracker_halfopen'] = "Nach Absprache";
+$l['inplaytracker_order'] = "Feste Postingreihenfolge";
+$l['inplaytracker_no_order'] = "Keine Reihenfolge";
+$l['inplaytracker_order_setting'] = "Postingreihenfolge";
+$l['inplaytracker_open_setting'] = "Szenen-Zugang";
+$l['inplaytracker_search_scene'] = "Szenen filtern";
+$l['inplaytracker_scene_no_order'] = "Für diese Szene existiert <strong>keine feste Postingreihenfolge</strong>! (Achtung: sie wird in Ihrem Tracker also niemals als offene Szene angezeigt!) Sie dürfen sich also austoben wie Sie möchten.";
+$l['inplaytracker_scene_open'] = "Bei dieser Szene handelt es sich um einen öffentlichen Thread! Sie haben also die Möglichkeit, Ihren Charakter";
+$l['inplaytracker_add_partner'] = "<i class=\"fa fa-hand-pointer-o\" aria-hidden=\"true\"></i> <strong>den Mitspielern hinzuzufügen</strong>";
+$l['inplaytracker_scene_halfopen'] = "Bei dieser Szene handelt es sich um einen halböffentlichen Thread! Bitte schreiben Sie den Threadersteller an, damit er Sie nach seinem Einverständnis in die Partnerliste eintragen kann!";
+$l['inplaytracker_partner_desc'] = "Fügen Sie Ihren eigenen Charakter nicht mit ein!<br />";
+$l['inplaytracker_partner_desc_2'] = "<br /> Für optimale Funktionsweise, fügen Sie bei mehreren Postpartnern die Namen in der entsprechenden Postreihenfolge ein.";
+$l['inplaytracker_location_desc'] = "Wo spielt Ihre Szene?";
+$l['inplaytracker_daytime_desc'] = "Zu welcher Tageszeit spielt Ihre Szene?";
+$l['inplaytracker_all_scenes'] = "Szenen insgesamt";
+$l['inplaytracker_open_scenes'] = "davon offen!";
+$l['ìnplaytracker_scenes_by'] = "Szenen von";
+$l['inplaytracker_next_post'] = "Nächster Post";
+$l['inplaytracker_scene_information'] = "Szenen-Informationen";
+$l['inplaytracker_last_post'] = "Letzter Post";
+$l['inplaytracker_characters'] = "Charaktere";
+
+// Alerts
+$l['inplaytracker_newthread'] = "{1} hat Ihnen eine neue Szene erstellt!";
+$l['inplaytracker_newreply'] = "{1} hat auf Ihre Szene \"{2}\" geantwortet!";
+$l['myalerts_setting_inplaytracker_newthread'] = "Benachrichtigung bei neu erstellten Szenen bekommen?";
+$l['myalerts_setting_inplaytracker_newreply'] = "Benachrichtigung bei neu erstellten Antworten auf Szenen bekommen?";
+
+?>
